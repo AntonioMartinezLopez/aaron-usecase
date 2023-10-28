51ce635757fb0d1420d2aebdbcbabd26e41e242e
Antonio Martinez Lopez <martinezlopez@outlook.de>
2023-10-28 20:49:46 +0200

    feat(frontend): add stat cards to display stats

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,34 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { API } from 'aws-amplify'
 import './App.css'
+import { useEffect, useState } from 'react';
+import { TypeInformationGermany } from '../../core/types/InformationGermany';
+import { TypeCases } from '../../core/types/InformationCases';
+
+function loadGermanyOverviewData(): Promise<TypeInformationGermany> {
+  return API.get('covid-api', "/api/germany", {});
+}
+
+function loadGermanyCaseData(days: number = 100) {
+  return API.get('covid-api', `/api/germany/cases/${days}`, {});
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setdata] = useState<TypeInformationGermany | null>(null);
+  const [caseData, setCaseData] = useState<TypeCases | null>(null);
+
+  useEffect(() => {
+
+    async function onLoad() {
+      try {
+        const notes = await loadGermanyOverviewData();
+        const caseData = await loadGermanyCaseData();
+        setdata(notes);
+        setCaseData(caseData);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    onLoad();
+
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="h-full gap-2 p-4 pb-4 pl-18 pr-18 overflow-y-auto">
+      <div className='bg-inherit w-full h-[100%] md:h-[50%] flex flex-col md:flex-row flex-wrap p-2'>
+        <div className='h-full flex-1 flex flex-col max-w-[50%]'>
+          <h1 className='text-2xl font-bold'>Covid-19 Statistics - Germany Today</h1>
+          <div className='flex-1 w-full flex flex-row p-2 gap-4 items-center justify-around' >
+
+            <div className="stats text-lg shadow h-32 lg:h-40 xl:h-46">
+              <div className="stat bg-slate-700">
+                <div className="stat-title">Total Number of Cases</div>
+                <div className="stat-value">{data?.cases}</div>
+                <div className="stat-desc text-sm">New Cases Today: {data?.delta.cases}</div>
+              </div>
+            </div>
+
+            <div className="stats text-lg shadow h-32 lg:h-40 xl:h-46">
+              <div className="stat bg-slate-700">
+                <div className="stat-title">Total Number of Deaths</div>
+                <div className="stat-value">{data?.deaths}</div>
+                <div className="stat-desc text-sm">New Deaths Today: {data?.delta.deaths}</div>
+              </div>
+            </div>
+
+            <div className="stats text-lg shadow h-32 lg:h-40 xl:h-46">
+              <div className="stat bg-slate-700">
+                <div className="stat-title">Week Incidence</div>
+                <div className="stat-value">{data?.weekIncidence.toPrecision(2)}</div>
+                <div className="stat-desc text-xs"> Yesterday:{data && data?.delta.weekIncidence > 0 ? '↗︎' : '↘︎'} {data?.delta.weekIncidence.toPrecision(2)}</div>
+              </div>
+            </div>
+
+            <div className="stats text-lg shadow h-32 lg:h-40 xl:h-46">
+              <div className="stat bg-slate-700">
+                <div className="stat-title">Recovered</div>
+                <div className="stat-value">{data?.recovered}</div>
+                <div className="stat-desc text-sm">Recovered today: {data?.delta.recovered}</div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+        <div className="divider md:divider-horizontal"></div>
+        <div className='h-full flex-1'></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='bg-inherit w-full h-[50%] p-2'>
+        <div className='h-full flex-1'></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import config from './config.ts';
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "covid-api",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
